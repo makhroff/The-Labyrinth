264bbed5455d1af9e7eeb188edc8307233bf4147
FeM <philippe.makhroff@gmail.com>
2023-04-01 16:34:26 +0200

    New better and cleaner code for movement

--- Program.cs
+++ Program.cs
@@ -1,13 +1,39 @@
 ﻿namespace The_Labyrinth
 {
-    public struct Vector2
+    public class Vector2
     {
         public int y;
         public int x;
+
+        public Vector2()
+        {
+            y = 0;
+            x = 0;
+        }
+
+        public Vector2(int x, int y)
+        {
+            this.y = y;
+            this.x = x;
+        }
+
+        public void Deconstruct(out int X, out int Y)
+        {
+            Y = y;
+            X = x;
+        }
     }
 
     static class Program
     {
+        public enum GameInput
+        {
+            MoveUp,
+            MoveDown,
+            MoveLeft,
+            MoveRight,
+        }
+
         private static Random random = new Random();
 
         private static int fieldDimensionX = 20;
@@ -17,10 +43,10 @@
         private static Vector2 finish = new Vector2();
         private static char[,] field = new char[fieldDimensionY, fieldDimensionX];
 
-        private static char playerChar = '@';
-        private static char wallChar = 'O';
-        private static char airChar = ' ';
-        private static char finishChar = 'F';
+        private const char playerChar = '@';
+        private const char wallChar = 'O';
+        private const char airChar = ' ';
+        private const char finishChar = 'F';
 
         private static double wallFrequency = 0.3;
 
@@ -28,12 +54,19 @@
 
         static void Main(string[] args)
         {
+            GameLoop();
+        }
+
+        static void GameLoop()
+        {
             InitPositions();
             InitField();
             DrawField();
             while (isGameRunning)
             {
-                TryToMovePlayer();
+                //MovePlayer();
+                var input = TryToCatchGameInput();
+                ProcessCachedInput(input);
             }
             Console.ReadKey();
         }
@@ -43,15 +76,6 @@
             player = GetRandomPosition();
             finish = GetRandomPosition();
         }
-
-        static Vector2 GetRandomPosition()
-        {
-            Vector2 position = new Vector2();
-            position.x = random.Next(0, fieldDimensionY);
-            position.y = random.Next(0, fieldDimensionX);
-            return position;
-        }
-
         static void InitField()
         {
             Console.Clear();
@@ -60,7 +84,7 @@
             {
                 for (int j = 0; j < fieldDimensionY; j++)
                 {
-                    field[j, i] = CreateLabirynthCell();
+                    field[j, i] = TryToCreateWall();
                 }
             }
 
@@ -68,72 +92,75 @@
             field[finish.x, finish.y] = finishChar;
         }
 
-        static void TryToMovePlayer()
+        static GameInput TryToCatchGameInput()
         {
-            switch (Console.ReadKey().Key)
+            while (true)
             {
-                case ConsoleKey.UpArrow:
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        return GameInput.MoveUp;
+                        
+                    case ConsoleKey.DownArrow:
+                        return GameInput.MoveDown;
 
-                    if (field[player.x, player.y - 1] == ' ')
-                    {
-                        field[player.x, player.y] = ' ';
-                        player.y -= 1;
+                    case ConsoleKey.LeftArrow:
+                        return GameInput.MoveLeft;
 
-                        DrawField();
-                    }
-                    else if (field[player.x, player.y - 1] == 'F')
-                    {
-                        Win();
-                    }
-
-                    break;
-                case ConsoleKey.DownArrow:
-
-                    if (field[player.x, player.y + 1] == ' ')
-                    {
-                        field[player.x, player.y] = ' ';
-                        player.y += 1;
-
-                        DrawField();
-                    }
-                    else if (field[player.x, player.y + 1] == 'F')
-                    {
-                        Win();
-                    }
-
-                    break;
-                case ConsoleKey.LeftArrow:
-
-                    if (field[player.x - 1, player.y] == ' ')
-                    {
-                        field[player.x, player.y] = ' ';
-                        player.x -= 1;
-
-                        DrawField();
-                    }
-                    else if (field[player.x - 1, player.y] == 'F')
-                    {
-                        Win();
-                    }
-
-                    break;
-                case ConsoleKey.RightArrow:
-
-                    if (field[player.x + 1, player.y] == ' ')
-                    {
-                        field[player.x, player.y] = ' ';
-                        player.x += 1;
-
-                        DrawField();
-                    }
-                    else if (field[player.x + 1, player.y] == 'F')
-                    {
-                        Win();
-                    }
-
-                    break;
+                    case ConsoleKey.RightArrow:
+                        return GameInput.MoveRight;
+                }
             }
         }
+
+        static void ProcessCachedInput(GameInput input)
+        {
+            bool playerIsMoving = (input == GameInput.MoveUp) || (input == GameInput.MoveDown) || (input == GameInput.MoveLeft) || (input == GameInput.MoveRight);
+
+            if (playerIsMoving)
+            {
+                var newCoords = CalculateNewPlayerCoordinates(input);
+
+                Console.WriteLine(newCoords.x + " " + newCoords.y);
+
+                if (!CoordsWithinField(newCoords)) return;
+                if (!CoordsLegal(newCoords)) return;
+
+                field[player.x, player.y] = airChar;
+                player = newCoords;
+
+                DrawField();
+            }
+        }
+
+        static Vector2 CalculateNewPlayerCoordinates(GameInput input) => input switch
+        {
+            GameInput.MoveUp => new(player.x, player.y - 1),
+            GameInput.MoveDown => new(player.x, player.y + 1),
+            GameInput.MoveLeft => new(player.x - 1, player.y),
+            GameInput.MoveRight => new(player.x + 1, player.y),
+            _ => throw new NotImplementedException("No correct format of player movement found.")
+        };
+
+        static bool CoordsWithinField(Vector2 coords)
+        {
+            if ((-1, coords.y) == (coords.x, coords.y)) return false;
+            if ((coords.x, -1) == (coords.x, coords.y)) return false;
+
+            if ((fieldDimensionY, coords.y) == (coords.x, coords.y)) return false;
+            if ((coords.x, fieldDimensionX) == (coords.x, coords.y)) return false;
+
+            return true;
+        }
+
+        static bool CoordsLegal(Vector2 coords) => GetCharFromField(coords) switch
+        {
+            wallChar => false,
+            finishChar => false,
+            _ => true
+        };
+
+        static char GetCharFromField(Vector2 coords) => field[coords.x, coords.y];
 
         static void DrawField()
         {
@@ -157,7 +184,7 @@
             Console.WriteLine("finish y:" + finish.y);
             Console.WriteLine("finish x:" + finish.x);
         }
-        static char CreateLabirynthCell()
+        static char TryToCreateWall()
         {
             if (random.NextDouble() <= wallFrequency)
             {
@@ -171,6 +198,13 @@
             isGameRunning = false;
             Console.Clear();
             Console.WriteLine("YOU WIN!");
+        }
+        static Vector2 GetRandomPosition()
+        {
+            Vector2 position = new Vector2();
+            position.x = random.Next(0, fieldDimensionY);
+            position.y = random.Next(0, fieldDimensionX);
+            return position;
         }
     }
 }
